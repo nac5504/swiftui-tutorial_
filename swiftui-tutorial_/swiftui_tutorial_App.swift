4f24eaba18b4adc237a363264bd9bf4480d80fce
Nicholas Candello <nac5504@gmail.com>
2022-09-29 18:34:44 -0400

    Initial Commit

---
+++ swiftui-tutorial_/swiftui_tutorial_App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorial_App.swift
+//  swiftui-tutorial_
+//
+//  Created by Nicholas Candello on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorial_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
